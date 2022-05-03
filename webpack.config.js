e1636e6988c0c105205ae35e8bdd629236f79d30
Lincoln Yan <yyjlincoln@gmail.com>
2022-05-03 22:45:22 +1000

    [chore] renamed to server.js

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   target: "node",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "index.js",
+    filename: "server.js",
   },
   resolve: {
     extensions: [".ts", ".js"],
