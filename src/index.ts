08cd87cdd54f1855ea6b09a760f742e9104caaa7
Lincoln Yan <yyjlincoln@gmail.com>
2022-05-03 23:57:22 +1000

    [feat] Finished POC

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,24 @@
-import { app, prisma, router } from "./server";
+import { app, prisma } from "./server";
 import { NOT_FOUND } from "./constants";
 
-const { PORT = 3030 } = process.env;
+const { PORT = 3030, CREATION_KEY } = process.env;
 
-router.use(async (req, res) => {
-  if (req.url === "/") {
+app.use(async (req, res, next) => {
+  if (req.path === "/") {
     res.redirect("https://yyjlincoln.com");
     return;
   }
-  const route = await prisma.urlMap.findFirst({ where: { shortUrl: req.url } });
+  if (req.path === "/create") {
+    if (req.query.key !== CREATION_KEY) {
+      res.status(403).send("Access is denied.");
+      return;
+    }
+    next();
+    return;
+  }
+  const route = await prisma.urlMap.findFirst({
+    where: { shortUrl: req.path },
+  });
   if (route) {
     res.redirect(route.url);
   } else {
@@ -16,7 +26,44 @@ router.use(async (req, res) => {
   }
 });
 
-app.use(router);
+app.get("/create", async (req, res) => {
+  const values = { ...req.body, ...req.params, ...req.query };
+  const { url, shortUrl } = values;
+  if (!url || !shortUrl) {
+    res
+      .status(400)
+      .send("Invalid request. Please include both url and shortUrl");
+    return;
+  }
+  try {
+    await prisma.urlMap.create({
+      data: {
+        url,
+        shortUrl,
+        createdAt: new Date(),
+      },
+    });
+    res.send("Successfully created.");
+  } catch (e) {
+    res.status(400).send(`Error creating url: ${e}`);
+  }
+});
+
+app.post("/create", (req, res) => {
+  const values = { ...req.body, ...req.params, ...req.query };
+  const { url, shortUrl } = values;
+  if (!url || !shortUrl) {
+    res.status(400).send("Invalid request");
+    return;
+  }
+  prisma.urlMap.create({
+    data: {
+      url,
+      shortUrl,
+      createdAt: new Date(),
+    },
+  });
+});
 
 async function main() {
   app.listen(PORT, () => {
